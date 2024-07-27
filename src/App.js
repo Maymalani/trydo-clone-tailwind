c735e534c92db8fd23b88e85c7b79467264f0559
Mayank Malani <mayankmalani0@gmail.com>
2024-07-27 14:05:50 +0530

    minor changes in navigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import Topnav from "./components/Topnav";
 import { Route, Routes } from "react-router-dom";
 import ClickToTop from "./components/ClickToTop";
 import Home from "./pages/Home";
-import Service from "./components/Service";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Team from "./components/Team";
-import Testimonial from "./components/Testimonial";
-import News from "./components/News";
-import ContactUs from "./components/ContactUs";
 
 function App() {
   return (
@@ -18,13 +11,6 @@ function App() {
       <Topnav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/#service" element={<Service />} />
-        <Route path="/#about" element={<About />} />
-        <Route path="/#portfolio" element={<Portfolio />} />
-        <Route path="/#team" element={<Team />} />
-        <Route path="/#testimonial" element={<Testimonial />} />
-        <Route path="/#blog" element={<News />} />
-        <Route path="/#contact" element={<ContactUs />} />
       </Routes>
     </>
   );
--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -40,13 +40,13 @@ const Topnav = () => {
                         </NavLink>
                         <nav id='nav' className='gap-x-7 ml-auto mr-10 text-[16px] font-medium font-poppins hidden xl:flex'>
                             <NavLink to="/" className="py-4 hover:text-[#f9004d]">Home</NavLink>
-                            <NavLink to="/#service" className="py-4 hover:text-[#f9004d]">Service</NavLink>
-                            <NavLink to="/#about" className="py-4 hover:text-[#f9004d]">About</NavLink>
-                            <NavLink to="/#portfolio" className="py-4 hover:text-[#f9004d]">Portfolio</NavLink>
-                            <NavLink to="/#team" className="py-4 hover:text-[#f9004d]">Team</NavLink>
-                            <NavLink to="/#testimonial" className="py-4 hover:text-[#f9004d]">Testimonial</NavLink>
-                            <NavLink to="/#blog" className="py-4 hover:text-[#f9004d]">Blog</NavLink>
-                            <NavLink to="/#contact" className="py-4 hover:text-[#f9004d]">Contact</NavLink>
+                            <NavLink to="#service" className="py-4 hover:text-[#f9004d]">Service</NavLink>
+                            <NavLink to="#about" className="py-4 hover:text-[#f9004d]">About</NavLink>
+                            <NavLink to="#portfolio" className="py-4 hover:text-[#f9004d]">Portfolio</NavLink>
+                            <NavLink to="#team" className="py-4 hover:text-[#f9004d]">Team</NavLink>
+                            <NavLink to="#testimonial" className="py-4 hover:text-[#f9004d]">Testimonial</NavLink>
+                            <NavLink to="#blog" className="py-4 hover:text-[#f9004d]">Blog</NavLink>
+                            <NavLink to="#contact" className="py-4 hover:text-[#f9004d]">Contact</NavLink>
                         </nav>
                         <div className='flex items-center gap-x-2'>
                             <button className={`hidden md:block bg-transparent px-4 py-2 border-[1px] ${scroll ? "text-black border-black" : "text-[#c6c9d8] border-[#c6c9d8]"}  rounded-md hover:bg-[#f9004d] hover:border-[#f9004d] hover:text-white`}>Buy Now</button>
@@ -56,13 +56,13 @@ const Topnav = () => {
                         </div>
                         <div className={`${show ? "block" : "hidden"} xl:hidden text-black overflow-y-auto rounded-md absolute top-[62px] left-0 w-full flex flex-col bg-white p-1`}>
                             <NavLink to="/" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 rounded-md">Home</NavLink>
-                            <NavLink to="/#service" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 my-1 rounded-md">Service</NavLink>
-                            <NavLink to="/#about" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 rounded-md">About</NavLink>
-                            <NavLink to="/#portfolio" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 my-1 rounded-md">Portfolio</NavLink>
-                            <NavLink to="/#team" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 rounded-md">Team</NavLink>
-                            <NavLink to="/#testimonial" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 my-1 rounded-md">Testimonial</NavLink>
-                            <NavLink to="/#blog" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 rounded-md">Blog</NavLink>
-                            <NavLink to="/#contact" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 my-1 rounded-md">Contact</NavLink>
+                            <NavLink to="#service" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 my-1 rounded-md">Service</NavLink>
+                            <NavLink to="#about" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 rounded-md">About</NavLink>
+                            <NavLink to="#portfolio" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 my-1 rounded-md">Portfolio</NavLink>
+                            <NavLink to="#team" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 rounded-md">Team</NavLink>
+                            <NavLink to="#testimonial" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 my-1 rounded-md">Testimonial</NavLink>
+                            <NavLink to="#blog" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 rounded-md">Blog</NavLink>
+                            <NavLink to="#contact" onClick={() => setShow(false)} className="py-1 hover:text-[#f9004d] px-2 my-1 rounded-md">Contact</NavLink>
                         </div>
                     </div>
                 </div>
